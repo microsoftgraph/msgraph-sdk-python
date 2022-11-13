0658d7970b9020de0e170953837810461446f1e4
samwelkanda <samwelkanda@gmail.com>
2022-11-14 02:11:14 +0300

    Add placeholder script to increment minor version

---
+++ scripts/incrementMinorVersion.ps1
@@ -0,0 +1,14 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+<#
+.Synopsis
+    Placeholder to Increment the minor version string if the major,
+    minor, or patch version hasn't been manually updated.
+.Description
+    Assumptions:
+        Targets _version file
+        This script assumes it is run from the repo root.
+        Minor version is typically auto-incremented.
+        TODO: Version number will be incremented after first release of the SDK
+#>

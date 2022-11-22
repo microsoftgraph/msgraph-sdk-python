d1b1a8ece2a7a5b3cba1329f9475ac0bb0c9be85
samwelkanda <samwelkanda@gmail.com>
2022-11-23 00:49:19 +0300

    Simplify package imports

--- msgraph/__init__.py
+++ msgraph/__init__.py
@@ -2,5 +2,7 @@
 Python client for the Microsoft Graph API
 """
 from ._version import VERSION
+from .graph_request_adapter import GraphRequestAdapter
+from .graph_service_client import GraphServiceClient
 
 __version__ = VERSION

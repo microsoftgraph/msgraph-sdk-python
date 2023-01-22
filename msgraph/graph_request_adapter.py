7eac6d933bac2dab3b5e700d7a0f5a7e91ab2bc5
samwelkanda <samwelkanda@gmail.com>
2023-01-23 01:14:03 +0300

    Add optional parameter for custom http client

--- msgraph/graph_request_adapter.py
+++ msgraph/graph_request_adapter.py
@@ -1,8 +1,10 @@
+from typing import Dict, Optional
+import httpx
 from kiota_abstractions.authentication import AuthenticationProvider
-from msgraph_core import BaseGraphRequestAdapter
+from msgraph_core import BaseGraphRequestAdapter, GraphClientFactory
 
 
 class GraphRequestAdapter(BaseGraphRequestAdapter):
-    def __init__(self, auth_provider: AuthenticationProvider) -> None:
-        super().__init__(auth_provider)
+    def __init__(self, auth_provider: AuthenticationProvider, client: Optional[httpx.AsyncClient] = GraphClientFactory.create_with_default_middleware()) -> None:
+        super().__init__(auth_provider, http_client=client)
 

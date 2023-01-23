787282082b72eb59b2903b5fec0a84942ba6b53e
samwelkanda <samwelkanda@gmail.com>
2023-01-23 18:03:17 +0300

    Accept custom client and request options

--- msgraph/graph_request_adapter.py
+++ msgraph/graph_request_adapter.py
@@ -1,10 +1,16 @@
 from typing import Dict, Optional
 import httpx
 from kiota_abstractions.authentication import AuthenticationProvider
-from msgraph_core import BaseGraphRequestAdapter, GraphClientFactory
+from msgraph_core import APIVersion, BaseGraphRequestAdapter, GraphClientFactory
+from msgraph_core.middleware.options import GraphTelemetryHandlerOption
+
+from ._version import VERSION
+
+options = {GraphTelemetryHandlerOption().get_key(): GraphTelemetryHandlerOption(api_version=APIVersion.v1, sdk_version=VERSION)}
 
 
 class GraphRequestAdapter(BaseGraphRequestAdapter):
-    def __init__(self, auth_provider: AuthenticationProvider, client: Optional[httpx.AsyncClient] = GraphClientFactory.create_with_default_middleware()) -> None:
+    def __init__(self, auth_provider: AuthenticationProvider,
+                 client: Optional[httpx.AsyncClient] = GraphClientFactory.create_with_default_middleware(options=options)) -> None:
         super().__init__(auth_provider, http_client=client)
 

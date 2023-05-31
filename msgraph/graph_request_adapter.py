1567bb8d721201e3d9bf7a4a2405b57187378532
samwelkanda <samwelkanda@gmail.com>
2023-05-31 16:58:16 +0300

    Add url replace options

--- msgraph/graph_request_adapter.py
+++ msgraph/graph_request_adapter.py
@@ -1,12 +1,21 @@
 from typing import Dict, Optional
 import httpx
 from kiota_abstractions.authentication import AuthenticationProvider
+from kiota_http.middleware.options import UrlReplaceHandlerOptions
 from msgraph_core import APIVersion, BaseGraphRequestAdapter, GraphClientFactory
 from msgraph_core.middleware.options import GraphTelemetryHandlerOption
 
 from ._version import VERSION
 
-options = {GraphTelemetryHandlerOption().get_key(): GraphTelemetryHandlerOption(api_version=APIVersion.v1, sdk_version=VERSION)}
+options = {
+    UrlReplaceHandlerOption.get_key(): UrlReplaceHandlerOptions(
+        enabled = True,
+        replacement_pairs = {"/users/me-token-to-replace": "/me"}
+    ),
+    GraphTelemetryHandlerOption.get_key(): GraphTelemetryHandlerOption(
+        api_version=APIVersion.v1,
+        sdk_version=VERSION)
+}
 
 
 class GraphRequestAdapter(BaseGraphRequestAdapter):

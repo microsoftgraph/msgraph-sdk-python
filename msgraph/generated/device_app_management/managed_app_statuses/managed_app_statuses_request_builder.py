6ed989aff3c4340860e05cd43a4d061da7d820a9
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-09-02 10:23:38 +0000

    feat(generation): update request builders and models
    Update generated files with build 196922

--- msgraph/generated/device_app_management/managed_app_statuses/managed_app_statuses_request_builder.py
+++ msgraph/generated/device_app_management/managed_app_statuses/managed_app_statuses_request_builder.py
@@ -49,10 +49,10 @@ class ManagedAppStatusesRequestBuilder(BaseRequestBuilder):
     
     async def get(self,request_configuration: Optional[RequestConfiguration[ManagedAppStatusesRequestBuilderGetQueryParameters]] = None) -> Optional[ManagedAppStatusCollectionResponse]:
         """
-        List properties and relationships of the managedAppStatusRaw objects.
+        List properties and relationships of the managedAppStatus objects.
         param request_configuration: Configuration for the request such as headers, query parameters, and middleware options.
         Returns: Optional[ManagedAppStatusCollectionResponse]
-        Find more info here: https://learn.microsoft.com/graph/api/intune-mam-managedappstatusraw-list?view=graph-rest-1.0
+        Find more info here: https://learn.microsoft.com/graph/api/intune-mam-managedappstatus-list?view=graph-rest-1.0
         """
         request_info = self.to_get_request_information(
             request_configuration
@@ -93,7 +93,7 @@ class ManagedAppStatusesRequestBuilder(BaseRequestBuilder):
     
     def to_get_request_information(self,request_configuration: Optional[RequestConfiguration[ManagedAppStatusesRequestBuilderGetQueryParameters]] = None) -> RequestInformation:
         """
-        List properties and relationships of the managedAppStatusRaw objects.
+        List properties and relationships of the managedAppStatus objects.
         param request_configuration: Configuration for the request such as headers, query parameters, and middleware options.
         Returns: RequestInformation
         """
@@ -139,7 +139,7 @@ class ManagedAppStatusesRequestBuilder(BaseRequestBuilder):
     @dataclass
     class ManagedAppStatusesRequestBuilderGetQueryParameters():
         """
-        List properties and relationships of the managedAppStatusRaw objects.
+        List properties and relationships of the managedAppStatus objects.
         """
         def get_query_parameter(self,original_name: str) -> str:
             """
